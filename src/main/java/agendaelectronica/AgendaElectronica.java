50a0e22c4bd22092d3aac4e1ce9f2103938d8172
crisMildenberger <milden.999@gmail.com>
2024-01-31 19:17:57 -0300

    Agregando

---
+++ src/main/java/agendaelectronica/AgendaElectronica.java
@@ -0,0 +1,14 @@
+package agendaelectronica;
+
+import agendaelectronica.use.Principal2;
+
+public class AgendaElectronica {
+
+    public static void main(String[] args) {
+       
+Principal2 pri = new Principal2();
+pri.setVisible(true);
+pri.setLocationRelativeTo(null);
+
+    }
+}
